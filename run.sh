b9ea29079bb8d1e14a68f848f8ce132db8105535
김기환 <kimkeehwan@gimgihwan-ui-MacBook-Pro.local>
2018-04-03 20:15:55 +0900

    a

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 i=$1
-./lwm2mclient -4 -l $((40000 + $i)) -n $HOSTNAME.$i -t 30 -h 172.31.23.6 >/dev/null </dev/null & disown
+./lwm2mclient -4 -l $((40000 + $i)) -n $HOSTNAME.$i -t 300 -h 172.31.23.6 >/dev/null </dev/null & disown
