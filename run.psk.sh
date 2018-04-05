57dcdde550003a94c45dffaee1d00444be8a14aa
김기환 <kimkeehwan@gimgihwan-ui-MacBook-Pro.local>
2018-04-05 11:23:51 +0900

    dm

--- run.psk.sh
+++ run.psk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 i=$1
-./lwm2mclient -4 -l $((40000 + $i)) -n $HOSTNAME.$i -t 300 -h 172.31.23.6 -p 5684 -i handy -s 0000 >/dev/null </dev/null & disown
+./lwm2mclient -4 -l $((40000 + $i)) -n $HOSTNAME.$i -t 300 -h 172.31.19.202 -p 5684 -i $HOSTNAME.$i -s 0000 >/dev/null </dev/null & disown
